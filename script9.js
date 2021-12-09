f498123b7c72f6a6a2b4e55e593469cb4140bbe8
Davit <90.davitmanukyan@mail.ru>
2021-12-09 20:29:15 +0400

    How to take arguments in sum function?

--- script9.js
+++ script9.js
@@ -1,8 +1,17 @@
 "use strict";
 
-const arr1 = [1, 2, 3, 4];
-const arr2 = [5, 6, 7, 8, 9];
-const arr3 = arr1.concat(arr2);
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [5, 6, 7, 8, 9];
+// const arr3 = arr1.concat(arr2);
 
-console.log(arr1);
-console.log(arr3);
+// console.log(arr1);
+// console.log(arr3);
+
+function sum(...args) {
+  return args.reduce((sum, current) => {
+    return sum + current;
+  });
+}
+
+console.log(sum(1, 2));
+console.log(sum(1, 2, 3));

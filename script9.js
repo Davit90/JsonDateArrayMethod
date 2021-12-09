13deb4c23e360ac06471fba933b9953338fd3b76
Davit <90.davitmanukyan@mail.ru>
2021-12-09 20:30:22 +0400

    How to pass arguments in sum function?

--- script9.js
+++ script9.js
@@ -7,11 +7,17 @@
 // console.log(arr1);
 // console.log(arr3);
 
-function sum(...args) {
-  return args.reduce((sum, current) => {
-    return sum + current;
-  });
-}
+// function sum(...args) {
+//   return args.reduce((sum, current) => {
+//     return sum + current;
+//   });
+// }
 
-console.log(sum(1, 2));
-console.log(sum(1, 2, 3));
+// console.log(sum(1, 2));
+// console.log(sum(1, 2, 3));
+
+function sum(x, y, z) {
+  return x + y + z;
+}
+const numbers = [1, 2, 3];
+console.log(sum(...numbers));
